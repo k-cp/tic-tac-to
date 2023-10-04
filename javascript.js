ef80dad8f55828af9c239f100c828e4b33f8c0b0
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2023-10-04 23:23:17 +0100

    Add eventlistener to blocks

--- javascript.js
+++ javascript.js
@@ -11,9 +11,21 @@ const Player = name => {
     return {name} 
 }
 
+function addChoice() {
+    if (this.innerHTML === '') {
+        const circle = document.createElement('i');
+        circle.className = 'bx bx-radio-circle';
+        this.appendChild(circle);
+    }
+}
+
 const container = document.querySelector('.container');
-for (let i = 0; i < 0; i++) {
-    const block = document.createElement('div');
+for (let i = 0; i < 9; i++) {
+    let block = document.createElement('div');
+    block.classList.add('block');
     block.textContent = gameBoard.gameboard[i];
     container.appendChild(block);
 };
+
+const blocks = document.querySelectorAll('.block');
+blocks.forEach((block) => block.addEventListener('click', addChoice))
