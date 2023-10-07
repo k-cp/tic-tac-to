99942ce394a76561cdcbc36aa15e743e2960180c
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2023-10-07 12:35:39 +0100

    Minor bug fix

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 //まずはゲームボード
 
-const gameBoard = {
-    gameboard : new Array(9)
-}
+const gameBoard = (() => {
+    const gameboard = new Array(9);
+    return {gameboard}
+})()
 
 //次はプレイヤー
 
@@ -11,11 +12,62 @@ const Player = name => {
     return {name} 
 }
 
+const form = document.getElementById('modeSelect');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+});
+
+function getMode() {
+    const option = document.getElementById('mode');
+    const playerOption = option.value;
+    const button = document.querySelector('.playBtn');
+    const restart = document.querySelector('.restartBtn');
+    button.style.display = 'none';
+    restart.style.display = 'block';
+
+    if (playerOption === 'pvp') {
+        playPvP()
+    }
+    else {
+        playPvE()
+    }
+};
+
+function restart() {
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach(block => block.innerHTML = '')
+}
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
+function playPvP() {
+    //
+    left.textContent = 'Player 1';
+    right.textContent = 'Player 2';
+    right.classList.add('notPlaying');
+}
+
+function playPvE() {
+    //
+}
+
 function addChoice() {
     if (this.innerHTML === '') {
-        const circle = document.createElement('i');
-        circle.className = 'bx bx-radio-circle';
-        this.appendChild(circle);
+        if (left.classList.contains('notPlaying')) {
+            const circle = document.createElement('i');
+            circle.className = 'bx bx-x';
+            this.appendChild(circle);
+            right.classList.toggle('notPlaying');
+            left.classList.toggle('notPlaying');
+        }
+        else {
+            const circle = document.createElement('i');
+            circle.className = 'bx bx-radio-circle';
+            this.appendChild(circle);
+            right.classList.toggle('notPlaying');
+            left.classList.toggle('notPlaying');
+        }
     }
 }
 
